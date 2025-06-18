854aeadd7292e48dba9560e7ceffe4bff02fddb2
prnsly <enobakhareprincely@gmail.com>
2025-06-18 12:09:43 +0100

    Initial Project Commit

---
+++ src/main/java/com/prins1y/quicktask/QuicktaskApplication.java
@@ -0,0 +1,13 @@
+package com.prins1y.quicktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicktaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuicktaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prins1y/quicktask/QuicktaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prins1y.quicktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicktaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
